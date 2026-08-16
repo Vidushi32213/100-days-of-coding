ff0fe3f666b58fbd833818ad5ae2b7e163ed1cb5
Vidushi32213 <Vidushi.32213@stu.upes.ac.in>
2026-08-16 21:18:41 +0530

    Add files via upload
    100 days of code
    Day 1) Ques no. 1
    Write a program to input two numbers and display their sum

---
+++ ccode1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+
+    printf("Enter 1st number: ");
+    scanf("%d", &a);
+
+    printf("Enter 2nd number: ");
+    scanf("%d", &b);
+
+    int c = a + b;
+
+    printf("Sum = %d", c);
+
+    return 0;
+}
